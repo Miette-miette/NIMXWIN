25c73c33526b051eaeeafe36f915a471efe22483
Doriane <127025783+Miette-miette@users.noreply.github.com>
2023-05-18 17:22:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let mikado =20;
 let maitreNeutre= document.getElementById("maitreNeutre");
 let maitreDefaite= document.getElementById("maitreDefaite");
+let choix=document.getElementById("level")
+
+function choix_difficulte()
+{
+   
+   choix.style.right="-100%";
+   let mikado_anim= document.querySelectorAll("#mikado li:nth-of-type(2n)");
+    for(let k=0; k<mikado_anim.length; k++)
+    {
+        mikado_anim[k].style.animation='mikado_start1 1s 1';
+    }
+    
+    let mikado_anim2= document.querySelectorAll("#mikado li:nth-of-type(2n+1)");
+    for(let j=0; j<mikado_anim2.length; j++)
+    {
+        mikado_anim2[j].style.animation='mikado_start2 1s 1';
+    }
+    console.log(mikado_anim2);
+}
 
 
 function nouveau_tour()
@@ -18,12 +37,12 @@ function tour_adverse()
 {
     setTimeout(function() 
     {
-        
-        let retrait=getRandomInt(2)+1;
+        let difficulte=(document.querySelector('input[name="niveau_difficulte"]:checked').value);
+        let retrait=choix_adverse(difficulte);
    
     while(retrait>mikado)
     {
-        retrait=getRandomInt(2)+1;
+        retrait=choix_adverse(difficulte);
         console.log("retry",retrait);
     }
 
@@ -40,7 +59,15 @@ function tour_adverse()
     nouveau_tour();
     document.getElementById("valider").disabled=false;
     }, 1000);
-    
+       
+}
+
+function choix_adverse(facilite=0){
+    let batonnet_group=mikado-1;
+    let ecart=batonnet_group-(Math.floor(batonnet_group/4)*4);
+     ecart=(ecart>0) ? ecart : 1;
+
+    return (getRandomInt(100)>facilite) ? ecart :getRandomInt(2)+1 ;
     
 }
 
@@ -48,21 +75,11 @@ function mise_en_place()
 { 
     maitreNeutre.style.display="block";
                 
-     maitreDefaite.style.display="none";
+    maitreDefaite.style.display="none";
     
+    choix.style.display="flex";
     nouveau_tour();
-    let mikado_anim= document.querySelectorAll("#mikado li:nth-of-type(2n)");
-    for(let k=0; k<mikado_anim.length; k++)
-    {
-        mikado_anim[k].style.animation='mikado_start1 1s infinite';
-    }
     
-    let mikado_anim2= document.querySelectorAll("#mikado li:nth-of-type(2n+1)");
-    for(let j=0; j<mikado_anim2.length; j++)
-    {
-        mikado_anim2[j].style.animation='mikado_start2 1s infinite';
-    }
-    console.log(mikado_anim2);
     
     setTimeout( tour_adverse(),2000);
     
@@ -112,5 +129,3 @@ function jouer()
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-
-
